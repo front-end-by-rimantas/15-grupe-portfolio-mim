06f895cf5c76f30c445f346fa77ea74e93b77754
eimantaspav <63236210+eimantaspav@users.noreply.github.com>
2020-05-27 14:50:38 +0300

    Istrinti console.log

--- js/home.js
+++ js/home.js
@@ -4,8 +4,6 @@ import skills from './data.js';
 
 import progressBars from './components/progress-bar.js';
 
-console.log(progressBars);
-
 const progressBarsSelector = '#skills .bars';
 progressBars.renderProgressBar(progressBarsSelector, skills);
 progressBars.animateProgressBars(progressBarsSelector);
