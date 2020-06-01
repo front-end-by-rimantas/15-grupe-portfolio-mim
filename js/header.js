6d72c20411c0fc49bf73a4ccca36a807f715cced
Aleksiukas <edgaras.aleksiukas@gmail.com>
2020-06-01 12:58:21 +0300

    sidebar add

--- js/header.js
+++ js/header.js
@@ -1,7 +1,19 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "318px";
+    document.getElementById("mySidepanel").style.width = "300px";
+    const x  = document.querySelectorAll('.add');
+    x[0].classList.add("add-active");
+    x[1].classList.add("add-active");
+
+
+    console.log(x);
+    
   }
   
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+    const x  = document.querySelectorAll('.add');
+    x[0].classList.remove("add-active");
+    x[1].classList.remove("add-active");
+
   }
+ 
